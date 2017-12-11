2d27ee124c85963fca5137bfd1ab9d89a4507743
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-11 18:54:19 +0100

    build: Add Pnw build support
     *Source: https://github.com/LineageOS/android_build/commit/614a27a388fd5d914ca97fb39766e860a7256556
     *Credits: luca020400
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -135,6 +135,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^pnw_") ; then
+        PNW_BUILD=$(echo -n $1 | sed -e 's/^pnw_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $PNW_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        PNW_BUILD=
+    fi
+    export PNW_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -603,6 +611,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
