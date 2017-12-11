7a1133852032da98b9d25c67322afa70385884a3
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-11 18:58:36 +0100

    core: Include Pnw pathmap
     *Source: https://github.com/LineageOS/android_build/commit/ea11fcadb759a45067c8a9fb4d53e93207589ab4
     *Credits: luca020400
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -99,3 +99,8 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+
+ifneq ($(PNW_BUILD),)
+-include $(TOPDIR)vendor/pnw/build/core/pathmap.mk
+endif
